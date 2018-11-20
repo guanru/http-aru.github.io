230fc898e59d960852eabda9413fd01700d07678
guanru <1030662776@qq.com>
2018-11-20 10:19:56 +0800

    Site updated: 2018-11-20 10:19:56

--- dist/music.js
+++ dist/music.js
@@ -8,5 +8,11 @@
             artist: '莫文蔚',
             url: 'http://music.163.com/song/media/outer/url?id=541687281.mp3',
             cover: 'http://p2.music.126.net/21l0Cy8SO6ycY_eZT-6vVw==/109951163386949943.jpg'
+        }],
+        audio: [{
+            name: '一生所爱',
+            artist: '莫文蔚',
+            url: 'http://music.163.com/song/media/outer/url?id=25707139.mp3',
+            cover: 'http://p1.music.126.net/ZwOyxtml-t4cra3hRy7CkQ==/6634453162971822.jpg'
         }]
     });
