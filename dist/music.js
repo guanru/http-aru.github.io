5e80aaaa23aa2641d4b9715217b26751d427f5d0
guanru <1030662776@qq.com>
2018-12-11 11:17:12 +0800

    Site updated: 2018-12-11 11:17:12

--- dist/music.js
+++ dist/music.js
@@ -7,7 +7,7 @@ const ap = new APlayer({
     audio: [{
         name: '慢慢喜欢你',
         artist: '莫文蔚',
-        url: 'http://music.163.com/song/media/outer/url?id=541687281.mp3',
-        cover: 'http://p2.music.126.net/21l0Cy8SO6ycY_eZT-6vVw==/109951163386949943.jpg'
+        url: 'https://music.163.com/song/media/outer/url?id=541687281.mp3',
+        cover: 'https://p2.music.126.net/21l0Cy8SO6ycY_eZT-6vVw==/109951163386949943.jpg'
     }]
 });
