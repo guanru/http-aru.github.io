ad24bac12cb27d3681f70faa37f53bd2adc89192
guanru <1030662776@qq.com>
2018-12-25 23:44:03 +0800

    Site updated: 2018-12-25 23:44:03

--- dist/music.js
+++ dist/music.js
@@ -2,14 +2,14 @@ const ap = new APlayer({
     container: document.getElementById('aplayer'),
     //mini: true,
     //fixed: true,
-    autoplay: false,
+    autoplay: true,
     //loop: 'all',
     preload: 'auto',
     mutex: true,
     audio: [{
         name: '慢慢喜欢你',
         artist: '莫文蔚',
-        url: 'http://music.163.com/song/media/outer/url?id=541687281.mp3',
+        url: 'https://music.163.com/song/media/outer/url?id=541687281.mp3',
         //url: 'https://music.163.com/#/song?id=541687281',
         cover: 'https://p1.music.126.net/21l0Cy8SO6ycY_eZT-6vVw==/109951163386949943.jpg'
     }]
